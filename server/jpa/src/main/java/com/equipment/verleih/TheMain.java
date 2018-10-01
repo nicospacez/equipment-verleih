a7d647eae44c25dd764393f9ffc77c0030aa8533
NicoZandomeneghi <nico@zandomeneghi.at>
2018-10-01 13:22:38 +0200

    Fixed #7 init jpa setup

---
+++ server/jpa/src/main/java/com/equipment/verleih/TheMain.java
@@ -0,0 +1,17 @@
+package com.equipment.verleih;
+
+/**
+ *
+ * @author nicoz
+ */
+public class TheMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+    }
+    
+}
