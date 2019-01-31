a6c48758b2afa0f0fa256294addae761a1a9bd63
Pascal Bachinger <pascal-bachinger@hotmail.com>
2019-01-31 10:08:26 +0100

    updated ci_WebClient

--- web-client/WebClient/src/entities.ts
+++
@@ -1,5 +0,0 @@
-export class User{
-    constructor(public id: number, public firstname: string, public lastname: string, public username: string, public password: string, public position: string){
-
-    }
-}
