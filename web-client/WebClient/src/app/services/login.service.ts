0a2d77068dbfe655c5196b35abe74656d2fe2630
Nico Zandomeneghi <nicozandomeneghi@outlook.de>
2019-03-26 01:38:01 +0100

    bugfix

--- web-client/WebClient/src/app/services/login.service.ts
+++ web-client/WebClient/src/app/services/login.service.ts
@@ -11,21 +11,19 @@ export class LoginService {
 
   private readonly baseurl = 'http://192.168.99.100:8080/jee/app/jwt';
 
-  public getTokenWithCredentials(username: string, password: string): Observable<boolean> {
-    const ret: Subject<boolean> = new Subject<boolean>();
-    const body = { username: username, password: password };
-    this.http.post(`${this.baseurl}`, body, { responseType: 'text' }).subscribe(
-      result => {
+
+  public getTokenWithCredentials(username: string, password: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      const body = { username: username, password: password };
+      this.http.post(`${this.baseurl}`, body, { responseType: 'text' }).subscribe(result => {
         console.log(result);
+        resolve(true)
         if (result) {
           localStorage.setItem('token', result);
-          ret.next(true);
         }
-        ret.next(false);
-      }
-    );
-
-    return ret;
+      },
+        (err) => resolve(false)
+      );
+    })
   }
-
 }
